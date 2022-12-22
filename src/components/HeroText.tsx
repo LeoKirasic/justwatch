cab35fa8605feadd334059fdee568cb433a6d4ba
Leo Kirasic <kirasicleo@gmail.com>
2022-12-22 23:47:36 +0100

    Create HeroText component

---
+++ src/components/HeroText.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroText() {
+  return (
+    <p className='text-center text-2xl w-[290px] font-bold'>
+      All your streaming platforms in one app
+    </p>
+  );
+}
+
+export default HeroText;
