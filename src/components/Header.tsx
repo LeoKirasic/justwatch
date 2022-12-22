304894d525bd04e1eb5ca47088dca7716caa589c
Leo Kirasic <kirasicleo@gmail.com>
2022-12-22 23:47:45 +0100

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import headerImage from '../assets/Header.png';
+
+function Header() {
+  return (
+    <img className='w-64 mt-3' src={headerImage} alt='Margins Watch'></img>
+  );
+}
+
+export default Header;
