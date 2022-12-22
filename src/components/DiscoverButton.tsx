3fe52cd18fda33c01dcb41ff655d86312b81e9b7
Leo Kirasic <kirasicleo@gmail.com>
2022-12-22 23:48:05 +0100

    Create DiscoverButton component

---
+++ src/components/DiscoverButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DiscoverButton() {
+  return (
+    <button className='bg-primary h-14 w-48 rounded-md text-xl font-medium text-black'>
+      Discover movies
+    </button>
+  );
+}
+
+export default DiscoverButton;
