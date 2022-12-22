bb80b8f4a64bf8d204ad42d46a68910e2fb985b9
Leo Kirasic <kirasicleo@gmail.com>
2022-12-22 23:47:14 +0100

    Create SubText component

---
+++ src/components/SubText.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SubText() {
+  return (
+    <div className='text-center w-[240px] text-light-gray'>
+      <p>Get personalized recommendations for movies</p>
+      <p>and series available on Netflix, Amazon Prime</p>
+      <p>Video, Ivi and more.</p>
+    </div>
+  );
+}
+
+export default SubText;
