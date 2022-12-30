9822a4bf5b95348a36476c7e771e82ea60830f74
Leo Kirasic <kirasicleo@gmail.com>
2022-12-30 19:50:33 +0100

    Add event handler

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import headerImage from '../../assets/Header.png';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
   return (
     <img
-      className='max-w-[150px] 2xl:max-w-[500px] md:max-w-[300px]'
+      className='max-w-[150px] 2xl:max-w-[500px] md:max-w-[300px] cursor-pointer'
       src={headerImage}
       alt='Margins Watch'
+      onClick={handleClick}
     ></img>
   );
 }
