035ba2f367c0c8cc57c238e479481fae3d70dc84
Leo Kirasic <kirasicleo@gmail.com>
2022-12-30 16:13:23 +0100

    Add event handlers

--- src/components/shared/Search.tsx
+++ src/components/shared/Search.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import searchIcon from '../../assets/Search.png';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function Search() {
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e: any) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    navigate(`/search/:q`, { state: { query: query } });
+    setQuery('');
+  };
+
   return (
-    <input
-      className='w-20 bg-transparent text-light-gray'
-      type='text'
-      name='search'
-      placeholder='Search'
-    />
+    <form onSubmit={handleSubmit}>
+      <input
+        className='w-20 bg-transparent text-light-gray'
+        type='text'
+        name='search'
+        placeholder='Search'
+        value={query}
+        onChange={handleChange}
+      />
+    </form>
   );
 }
 
